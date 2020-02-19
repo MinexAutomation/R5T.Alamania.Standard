d75cbf2abdfe221960a29be64e3ae5984f2b6f32
MinexAutomation <minex.automation@gmail.com>
2020-02-18 22:15:40 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Alamania.Standard/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Bulgaria.UserProfileDirectory;
+using R5T.Costobocia.Standard;
+using R5T.Dacia;
+
+using R5T.Alamania.Bulgaria;
+
+
+namespace R5T.Alamania.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="IRivetOrganizationDirectoryPathProvider"/> service.
+        /// </summary>
+        public static IServiceCollection AddRivetOrganizationDirectoryPathProvider(this IServiceCollection services)
+        {
+            services.AddBulgariaRivetOrganizationDirectoryPathProvider(
+                services.AddDropboxDirectoryPathProviderAction(),
+                services.AddOrganizationStringlyTypedPathOperatorAction());
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="IRivetOrganizationDirectoryPathProvider"/> service.
+        /// </summary>
+        public static ServiceAction<IRivetOrganizationDirectoryPathProvider> AddRivetOrganizationDirectoryPathProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IRivetOrganizationDirectoryPathProvider>(() => services.AddRivetOrganizationDirectoryPathProvider());
+            return serviceAction;
+        }
+    }
+}
